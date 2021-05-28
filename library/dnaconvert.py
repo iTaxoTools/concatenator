83e540ae03c40d9bc804b9a8e6b11027b3fdc483
Vladimir Kharchev <necrosovereign@gmail.com>
2021-05-28 15:35:38 +0200

    DNAconvert wrapper function

--- library/dnaconvert.py
+++ library/dnaconvert.py
@@ -11,7 +11,7 @@ resource_path = getattr(sys, '_MEIPASS', sys.path[0])
 
 try:
     dnaconvert_path = None
-    with open(os.path.join(resource_path, "options.tab")) as options:
+    with open(os.path.join(resource_path, "data", "options.tab")) as options:
         for line in options:
             option, _, value = line.rstrip().partition("\t")
             if option == "DNAconvert":
@@ -22,20 +22,21 @@ except FileNotFoundError:
 def process(infile: TextIO, outfile: TextIO, format: str) -> None:
     if not dnaconvert_path:
         raise ValueError("DNAconvert path is not specified")
-    dna_input = tempfile.NamedTemporaryFile(mode="w")
-    dna_output = tempfile.NamedTemporaryFile(mode="r")
-    for line in infile:
-        dna_input.write(line)
+    _, dna_input = tempfile.mkstemp()
+    _, dna_output = tempfile.mkstemp()
+    dnaconvert_dir, _ = os.path.split(dnaconvert_path)
+    with open(dna_input, mode="w") as dna_infile:
+        for line in infile:
+            dna_infile.write(line)
     try:
-        subprocess.run(["dnaconvert_path", "--cmd", "--informat", "tab", "--outformat", format, dna_input.name, dna_output.name])
-    except FileNotFoundError:
-        raise ValueError(f"File {dnaconvert_path} doesn't exist")
-    except PermissionError:
-        raise ValueError(f"File {dnaconvert_path} is not executable")
+        subprocess.run([dnaconvert_path, "--cmd", "--informat", "tab", "--outformat", format, dna_input, dna_output], cwd=dnaconvert_dir)
+    except FileNotFoundError as e:
+        raise ValueError(f"File {dnaconvert_path} doesn't exist") from e
+    except PermissionError as e:
+        raise ValueError(f"File {dnaconvert_path} is not executable") from e
     else:
-        for line in dna_output:
-            outfile.write(line)
-    dna_input.close()
-    dna_output.close()
-    os.unlink(dna_input.name)
-    os.unlink(dna_output.name)
+        with open(dna_output) as dna_outfile:
+            for line in dna_outfile:
+                outfile.write(line)
+    os.unlink(dna_input)
+    os.unlink(dna_output)
