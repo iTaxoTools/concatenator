8be7b5c58f2f5a7646d2239029d4ed51a4540492
Vladimir Kharchev <necrosovereign@gmail.com>
2021-06-04 17:33:10 +0200

    Moved sequence padding to library/utils.py

--- library/utils.py
+++ library/utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from typing import Optional
+
 import pandas as pd
 
 
@@ -17,3 +19,29 @@ def into_seqids(table: pd.DataFrame) -> pd.Series:
         # Replace substrings of unallowed characters in the middle with _
         table[column] = table[column].str.replace(r"[^A-Za-z0-9]+", "_")
     return table.iloc[:, 0].str.cat(table.iloc[:, 1:], sep="_")
+
+
+def max_length_if_not_same(column: pd.Series) -> Optional[int]:
+    """
+    Returns length of the longest string in the ``column``, if the strings have different length
+    """
+    max_length = column.str.len().max()
+    min_length = column.str.len().min()
+    if max_length > min_length:
+        return max_length
+    else:
+        return None
+
+
+def make_equal_length(column: pd.Series, max_length=None) -> pd.Series:
+    """
+    Pads strings in the ``column`` to ``max_length``.
+
+    If ``max_length`` is None, uses the length of the longest string
+    """
+    if not max_length:
+        max_length = column.str.len().max()
+    if max_length:
+        return column.str.rjust(max_length, "N")
+    else:
+        return column
