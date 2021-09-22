e14836e201fd2927416afe0903e5ca30d25b4db4
StefanPatman <stefanpatman91@gmail.com>
2021-09-22 18:27:56 +0300

    Fix dnaconvert process
    Using os.unlink was throwing an error on Windows.
    Switched from mkstemp to the more convenient TemporaryDirectory
    within a context manager for automatic cleanup.

--- src/itaxotools/concatenator/library/dnaconvert.py
+++ src/itaxotools/concatenator/library/dnaconvert.py
@@ -5,16 +5,11 @@ from typing import TextIO
 import sys
 import os
 import tempfile
+import pathlib
 
 from itaxotools.DNAconvert import convertDNA, parse_format
 
-
-def process(infile: TextIO, outfile: TextIO, format: str) -> None:
-    _, dna_input = tempfile.mkstemp()
-    _, dna_output = tempfile.mkstemp()
-    with open(dna_input, mode="w") as dna_infile:
-        for line in infile:
-            dna_infile.write(line)
+def _dnaconvert(dna_input: str, dna_output: str, format: str) -> None:
     tabformat = parse_format("tab", ("", ""))
     assert tabformat is not None
     outformat = parse_format(format, ("", ""))
@@ -23,8 +18,18 @@ def process(infile: TextIO, outfile: TextIO, format: str) -> None:
         convertDNA(dna_infile, dna_outfile, tabformat, outformat,
                    allow_empty_sequences=False, disable_automatic_renaming=False)
 
-    with open(dna_output) as dna_outfile:
-        for line in dna_outfile:
-            outfile.write(line)
-    os.unlink(dna_input)
-    os.unlink(dna_output)
+def process(infile: TextIO, outfile: TextIO, format: str) -> None:
+    with tempfile.TemporaryDirectory(prefix='dnaconvert_') as dir:
+        path = pathlib.Path(dir)
+        dna_input = str(path / 'input')
+        dna_output = str(path / 'output')
+
+        with open(dna_input, mode="w") as dna_infile:
+            for line in infile:
+                dna_infile.write(line)
+
+        _dnaconvert(dna_input, dna_output, format)
+
+        with open(dna_output) as dna_outfile:
+            for line in dna_outfile:
+                outfile.write(line)
