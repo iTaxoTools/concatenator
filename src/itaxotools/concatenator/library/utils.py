d19eb4800cc2513704c44ac13bedeb8f02d953cc
StefanPatman <stefanpatman91@gmail.com>
2021-10-09 02:17:17 +0300

    Update utils.into_seqids
    Addresses FutureWarning about regex default value

--- src/itaxotools/concatenator/library/utils.py
+++ src/itaxotools/concatenator/library/utils.py
@@ -20,11 +20,11 @@ def into_seqids(table: pd.DataFrame) -> pd.Series:
     """
     for column in table.columns:
         # Remove unallowed character in the beginning
-        table[column] = table[column].str.replace(r"^[^A-Za-z09]+", "", n=1)
+        table[column] = table[column].str.replace(r"^[^A-Za-z0-9]+", "", n=1, regex=True)
         # Remove unallowed character in the end
-        table[column] = table[column].str.replace(r"[^A-Za-z0-9]+$", "", n=1)
+        table[column] = table[column].str.replace(r"[^A-Za-z0-9]+$", "", n=1, regex=True)
         # Replace substrings of unallowed characters in the middle with _
-        table[column] = table[column].str.replace(r"[^A-Za-z0-9]+", "_")
+        table[column] = table[column].str.replace(r"[^A-Za-z0-9]+", "_", regex=True)
     return table.iloc[:, 0].str.cat(table.iloc[:, 1:], sep="_")
 
 
