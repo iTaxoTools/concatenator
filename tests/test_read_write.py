4b76584eb23406f0add5466599c9d551df91e69f
StefanPatman <stefanpatman91@gmail.com>
2021-10-22 17:11:47 +0300

    Parametrize test_read_write per testfile
    Add an Ali zip to the tests

--- tests/test_read_write.py
+++ tests/test_read_write.py
@@ -5,8 +5,9 @@ from pathlib import Path
 import pytest
 
 from itaxotools.concatenator import autodetect, read_from_path, write_to_path, FileType
+from itaxotools.concatenator.library.file_utils import ZipPath
 
-TEST_DATA_DIR = Path("tests") / "data"  # I'm not sure if this is a good idea
+TEST_DATA_DIR = Path(__file__).parent / "data" / "sequences"
 
 
 def assert_eq_files(type: FileType, file1: Path, file2: Path) -> None:
@@ -14,18 +15,16 @@ def assert_eq_files(type: FileType, file1: Path, file2: Path) -> None:
         for (part1, part2) in zip(sorted(file1.iterdir()), sorted(file2.iterdir())):
             assert part1.read_text() == part2.read_text()
     elif type == FileType.ZipArchive:
-        for (part1, part2) in zip(sorted(file1.iterdir()), sorted(file2.iterdir())):
+        for (part1, part2) in zip(sorted(ZipPath(file1).iterdir()), sorted(ZipPath(file2).iterdir())):
             assert part1.read_text() == part2.read_text()
     else:
         assert file1.read_text() == file2.read_text()
 
 
-@pytest.mark.parametrize("data_dir", TEST_DATA_DIR.iterdir())
-def test_read_write(data_dir: Path, tmp_path: Path) -> None:
-    for input_file in data_dir.iterdir():
-        for expected_output in data_dir.iterdir():
-            expected_type, expected_format = autodetect(expected_output)
-            test_path = tmp_path / f"test_{expected_type}_{expected_format}"
-            write_to_path(read_from_path(input_file), test_path,
-                          expected_type, expected_format)
-            assert_eq_files(expected_type, test_path, expected_output)
+@pytest.mark.parametrize("expected_output", TEST_DATA_DIR.iterdir())
+def test_read_write(expected_output: Path, tmp_path: Path) -> None:
+    expected_type, expected_format = autodetect(expected_output)
+    test_path = tmp_path / f"test_{expected_type}_{expected_format}"
+    write_to_path(read_from_path(expected_output), test_path,
+                  expected_type, expected_format)
+    assert_eq_files(expected_type, test_path, expected_output)
