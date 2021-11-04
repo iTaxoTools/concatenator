72a1713773ee3348bc285e7f0310666261aa76dd
StefanPatman <stefanpatman91@gmail.com>
2021-11-04 15:26:51 +0200

    Update codon tests

--- tests/test_codons.py
+++ tests/test_codons.py
@@ -4,8 +4,11 @@ from enum import IntEnum
 import pandas as pd
 import pytest
 
-from itaxotools.concatenator.library.codons import GeneData, GeneticCode, ReadingFrame, BadReadingFrame
-
+from itaxotools.concatenator.library.model import GeneSeries
+from itaxotools.concatenator.library.codons import (
+    GeneticCode, ReadingFrame, NoReadingFrames,
+    BadReadingFrame, AmbiguousReadingFrame)
+from itaxotools.concatenator.library.operators import OpDetectReadingFrame
 
 @pytest.fixture
 def series_good() -> pd.Series:
@@ -28,55 +31,55 @@ def series_bad() -> pd.Series:
 
 
 @pytest.fixture
-def gene_good_standard_1st(series_good) -> GeneData:
-    data = GeneData(series_good)
+def gene_good_standard_1st(series_good) -> GeneSeries:
+    data = GeneSeries(series_good)
     data.genetic_code = GeneticCode(1)
     data.reading_frame = ReadingFrame(1)
     return data
 
 
 @pytest.fixture
-def gene_good_standard_unknown(series_good) -> GeneData:
-    data = GeneData(series_good)
+def gene_good_standard_unknown(series_good) -> GeneSeries:
+    data = GeneSeries(series_good)
     data.genetic_code = GeneticCode(1)
     data.reading_frame = ReadingFrame(0)
     return data
 
 
 @pytest.fixture
-def gene_good_unknown_unknown(series_good) -> GeneData:
-    data = GeneData(series_good)
+def gene_good_unknown_unknown(series_good) -> GeneSeries:
+    data = GeneSeries(series_good)
     data.genetic_code = GeneticCode(0)
     data.reading_frame = ReadingFrame(0)
     return data
 
 
 @pytest.fixture
-def gene_bad_standard_1st(series_bad) -> GeneData:
-    data = GeneData(series_bad)
+def gene_bad_standard_1st(series_bad) -> GeneSeries:
+    data = GeneSeries(series_bad)
     data.genetic_code = GeneticCode(1)
     data.reading_frame = ReadingFrame(1)
     return data
 
 
 def test_good_standard_1st(gene_good_standard_1st):
-    result = gene_good_standard_1st.detect_reading_frame()
+    result = OpDetectReadingFrame()(gene_good_standard_1st)
     assert result.reading_frame == 1
 
 
 def test_good_standard_unknown(gene_good_standard_unknown):
-    result = gene_good_standard_unknown.detect_reading_frame()
+    result = OpDetectReadingFrame()(gene_good_standard_unknown)
     assert result.reading_frame == 1
 
 
 def test_good_unknown_unknown(gene_good_unknown_unknown):
-    result = gene_good_unknown_unknown.detect_reading_frame()
+    result = OpDetectReadingFrame()(gene_good_unknown_unknown)
     assert result.reading_frame == 1
 
 
 def test_bad_standard_1st(gene_bad_standard_1st):
     with pytest.raises(BadReadingFrame):
-        gene_bad_standard_1st.detect_reading_frame()
+        OpDetectReadingFrame()(gene_bad_standard_1st)
 
 
 def test_model_reading_frame():
