72bdf516b069cb2684772cf31a57ab5ec8181f04
StefanPatman <stefanpatman91@gmail.com>
2021-11-08 20:50:32 +0200

    Update read/write tests

--- tests/test_read_write.py
+++ tests/test_read_write.py
@@ -40,13 +40,15 @@ self_test_data = [
     SelfTest(File('sequences_ali', FileType.Directory, FileFormat.Ali), {}, {}),
     SelfTest(File('sequences_ali.zip', FileType.ZipArchive, FileFormat.Ali), {}, {}),
     SelfTest(File('sequences.phy', FileType.File, FileFormat.Phylip), {}, {}),
-    SelfTest(File('sequences.fas', FileType.File, FileFormat.Fasta), {}, {}),
+    SelfTest(File('sequences.fas', FileType.File, FileFormat.Fasta), {},
+        dict(padding='')),
     SelfTest(File('sequences.tsv', FileType.File, FileFormat.Tab), {}, {}),
-    SelfTest(File('sequences.nex', FileType.File, FileFormat.Nexus), {}, {}),
+    SelfTest(File('sequences.nex', FileType.File, FileFormat.Nexus), {},\
+        dict(padding='N')),
     SelfTest(File('sequences_pad.fas', FileType.File, FileFormat.Fasta), {},
         dict(padding='-')),
     SelfTest(File('sequences_right_tab.nex', FileType.File, FileFormat.Nexus), {},
-        dict(justification=Justification.Right, separator='\t')),
+        dict(justification=Justification.Right, separator='\t', padding='N')),
 ]
 
 test_data = [
