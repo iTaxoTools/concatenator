05d8944859f19def075683c21405fd526bde7540
Vladimir Kharchev <necrosovereign@gmail.com>
2022-04-04 18:06:34 +0200

    Info grouped by input files

--- src/itaxotools/concatenator/general_info.py
+++ src/itaxotools/concatenator/general_info.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
+from typing import Iterator
 from enum import Enum, auto
 from dataclasses import dataclass
 
 import pandas as pd
 import numpy as np
+
+from .file_types import FileFormat
 
 
 class InfoColumns(Enum):
@@ -74,6 +77,7 @@ class GeneralInfo:
         result["% missing data"] = (
             dataframe[InfoColumns.MissingCount].sum()
             / dataframe[InfoColumns.NucleotideCount].sum()
+            * 100
         )
         result["GC content of sequences"] = (
             dataframe[InfoColumns.GCCount].sum()
@@ -107,3 +111,48 @@ class GeneralInfo:
         result["Maximum number of taxa per marker"] = taxa_per_marker.max()
         result["Average number of taxa per marker"] = taxa_per_marker.mean()
         return result
+
+    @staticmethod
+    def by_input_file(tables: Iterator[FileGeneralInfo]) -> pd.DataFrame:
+        rows = []
+        for file_info in tables:
+            row = file_info.table.dataframe.reset_index().agg(
+                {
+                    InfoColumns.Taxon: "nunique",
+                    InfoColumns.Gene: "nunique",
+                    InfoColumns.MissingCount: "sum",
+                    InfoColumns.NucleotideCount: "sum",
+                    InfoColumns.SeqLenMin: "min",
+                    InfoColumns.SeqLenMax: "max",
+                    InfoColumns.GCCount: "sum",
+                }
+            )
+            row["input file name"] = file_info.filename
+            row["input file format"] = file_info.file_format
+            rows.append(row)
+        result = pd.DataFrame(rows)
+        result[InfoColumns.MissingCount] = (
+            result[InfoColumns.MissingCount] / result[InfoColumns.NucleotideCount] * 100
+        )
+        result[InfoColumns.GCCount] = (
+            result[InfoColumns.GCCount] / result[InfoColumns.NucleotideCount]
+        )
+        result.drop(columns=InfoColumns.NucleotideCount, inplace=True)
+        result.rename(
+            columns={
+                InfoColumns.Taxon: "number of taxa",
+                InfoColumns.Gene: "number of genes (markers)",
+                InfoColumns.MissingCount: "% missing data",
+                InfoColumns.SeqLenMin: "sequence length minimum",
+                InfoColumns.SeqLenMax: "sequence length maximum",
+                InfoColumns.GCCount: "GC content of sequences",
+            }
+        )
+        return result
+
+
+@dataclass
+class FileGeneralInfo:
+    filename: str
+    file_format: FileFormat
+    table: GeneralInfo
